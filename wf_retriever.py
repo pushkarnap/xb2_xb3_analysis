e5453af76cb3dd6d12787af4d4213462f8de1859
Paarangat Pushkarna <paarangat.pushkarna@gmail.com>
2022-06-27 19:22:21 +1000

    h5py context manager used to retrieve the appropriate channel, argparse error fixed

--- wf_retriever.py
+++ wf_retriever.py
@@ -6,16 +6,23 @@ Reaches into an HDF file, goes to the specified pulse and waveform, returns.
 """
 
 import argparse 
+import h5py
 
 parser = argparse.ArgumentParser(description = 'Plot event data waveforms')
-parser.add_argument('-f', 'filepath', type=str, metavar='', required = True, 
+parser.add_argument('-f', '--filepath', type=str, metavar='', required = True, 
                     help = 'HDF filepath')
-parser.add_argument('-p', 'pulse', type=str, metavar='', required = True, 
+parser.add_argument('-p', '--pulse', type=str, metavar='', required = True, 
                     help = 'Name of pulse')
-parser.add_argument('-c', 'channel', type=str, metavar='', required = True,
+parser.add_argument('-c', '--channel', type=str, metavar='', required = True,
                     help = 'Name of channel')
 
 args = parser.parse_args()
 
-
+def return_wf(filepath, pulse, channel):
+    
+    with h5py.File(filepath, "r") as fhand:
+        print(fhand[f"{pulse}/{channel}"])
+        
+if __name__ == '__main__':
+    return_wf(args.filepath, args.pulse, args.channel)
 
